76d178aa73de849cba5a31a273c747542a1a93c8
joshsanyal <josh.sanyal@yahoo.com>
2024-10-16 15:12:35 -0700

    another guess for the fk

--- lab_4/lab_4.py
+++ lab_4/lab_4.py
@@ -113,6 +113,15 @@ class InverseKinematics(Node):
         T_RF_3_ee = translation(-0.06231, 0.06216, 0.01800)
         T_RF_0_ee = T_RF_0_1 @ T_RF_1_2 @ T_RF_2_3 @ T_RF_3_ee
         return T_RF_0_ee[:3, 3]
+    
+    # another guess?
+    # def fl_leg_fk(self, theta):
+    #     T_RF_0_1 = translation(0.07500, 0.04450, 0) @ rotation_x(-1.57080) @ rotation_z(theta[0])
+    #     T_RF_1_2 = translation(0.0, 0.0, 0.039) @ rotation_y(-1.57080) @ rotation_z(theta[1])
+    #     T_RF_2_3 = translation(0, 0.04940, 0.06850) @ rotation_y(1.57080) @ rotation_z(theta[2])
+    #     T_RF_3_ee = translation(0.06231, 0.06216, 0.01800)
+    #     T_RF_0_ee = T_RF_0_1 @ T_RF_1_2 @ T_RF_2_3 @ T_RF_3_ee
+    #     return T_RF_0_ee[:3, 3]
 
     def br_leg_fk(self, theta):
         T_RF_0_1 = translation(-0.07500, -0.0335, 0) @ rotation_x(1.57080) @ rotation_z(theta[0])
