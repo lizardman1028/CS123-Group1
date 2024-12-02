26496b72f2c08c5f7cec9edffb49e2209e24de1e
Sherry Xie <sherryxie@Sherrys-MacBook-Pro.local>
2024-12-02 15:56:58 -0800

    Modified drawing code

--- pupper_llm/pupper_llm/karel/karel.py
+++ pupper_llm/pupper_llm/karel/karel.py
@@ -6,80 +6,89 @@ from geometry_msgs.msg import Twist
 import simpleaudio as sa
 import pygame
 
+
 class KarelPupper:
-    def start():
-        rclpy.init()
+   def start():
+       rclpy.init()
 
-    def __init__(self):
-        # rclpy.init()
-        self.node = Node('karel_node')
-        self.publisher = self.node.create_publisher(Twist, 'cmd_vel', 10)
 
-    def move(self):
-        move_cmd = Twist()
-        move_cmd.linear.x = 1.0
-        move_cmd.angular.z = 0.0 
-        self.publisher.publish(move_cmd)
-        rclpy.spin_once(self.node, timeout_sec=1.0)
-        self.node.get_logger().info('Move forward...')
-        time.sleep(1)
-        self.stop()
+   def __init__(self):
+       # rclpy.init()
+       self.node = Node('karel_node')
+       self.publisher = self.node.create_publisher(Twist, 'cmd_vel', 10)
 
-    def turn(self, angle):
-        pass
-        move_cmd = Twist()
-        move_cmd.linear.x = 0.0
-        move_cmd.angular.z = angle
-        self.publisher.publish(move_cmd)
-        rclpy.spin_once(self.node, timeout_sec=1.0)
-        self.node.get_logger().info('Turn left...')
-        time.sleep(1)
-        self.stop()
 
-    def turn_left(self):
-        pass
-        move_cmd = Twist()
-        move_cmd.linear.x = 0.0
-        move_cmd.angular.z = 1.5 
-        self.publisher.publish(move_cmd)
-        rclpy.spin_once(self.node, timeout_sec=1.0)
-        self.node.get_logger().info('Turn left...')
-        time.sleep(1)
-        self.stop()
+   def move(self):
+       move_cmd = Twist()
+       move_cmd.linear.x = 1.0
+       move_cmd.angular.z = 0.0
+       self.publisher.publish(move_cmd)
+       rclpy.spin_once(self.node, timeout_sec=1.0)
+       self.node.get_logger().info('Move forward...')
+       time.sleep(1)
+       self.stop()
 
-    def turn_right(self):
-        move_cmd = Twist()
-        move_cmd.linear.x = 0.0
-        move_cmd.angular.z = -1.5 
-        self.publisher.publish(move_cmd)
-        rclpy.spin_once(self.node, timeout_sec=1.0)
-        self.node.get_logger().info('Turn right...')
+
+   def turn(self, radians):
+       pass
+       move_cmd = Twist()
+       move_cmd.linear.x = 0.0
+       move_cmd.angular.z = radians
+       self.publisher.publish(move_cmd)
+       rclpy.spin_once(self.node, timeout_sec=1.0)
+       self.node.get_logger().info('Turn left...')
+       time.sleep(1)
+       self.stop()
+
+
+   def turn_left(self):
+       pass
+       move_cmd = Twist()
+       move_cmd.linear.x = 0.0
+       move_cmd.angular.z = 1.5
+       self.publisher.publish(move_cmd)
+       rclpy.spin_once(self.node, timeout_sec=1.0)
+       self.node.get_logger().info('Turn left...')
+       time.sleep(1)
+       self.stop()
+
+
+   def turn_right(self):
+       move_cmd = Twist()
+       move_cmd.linear.x = 0.0
+       move_cmd.angular.z = -1.5
+       self.publisher.publish(move_cmd)
+       rclpy.spin_once(self.node, timeout_sec=1.0)
+       self.node.get_logger().info('Turn right...')
 #        time.sleep(0.5)
-        self.stop()
+       self.stop()
 
-    def bark(self):
-        self.node.get_logger().info('Bark...')
-        pygame.mixer.init()
-        bark_sound = pygame.mixer.Sound('/home/pi/pupper_llm/sounds/dog_bark.wav')
-        bark_sound.play()
-        
+
+   def bark(self):
+       self.node.get_logger().info('Bark...')
+       pygame.mixer.init()
+       bark_sound = pygame.mixer.Sound('/home/pi/pupper_llm/sounds/dog_bark.wav')
+       bark_sound.play()
+      
 #        time.sleep(0.5)
-        self.stop()
+       self.stop()
 
 
-    def stop(self):
-        self.node.get_logger().info('Stopping...')
-        move_cmd = Twist()
-        move_cmd.linear.x = 0.0
-        move_cmd.linear.y = 0.0
-        move_cmd.linear.z = 0.0
-        move_cmd.angular.x = 0.0
-        move_cmd.angular.y = 0.0
-        move_cmd.angular.z = 0.0
-        self.publisher.publish(move_cmd)
-        rclpy.spin_once(self.node, timeout_sec=1.0)
-    
-    def __del__(self):
-        self.node.get_logger().info('Tearing down...')
-        self.node.destroy_node()
-        rclpy.shutdown()
+
+
+   def stop(self):
+       self.node.get_logger().info('Stopping...')
+       move_cmd = Twist()
+       move_cmd.linear.x = 0.0
+       move_cmd.linear.y = 0.0
+       move_cmd.linear.z = 0.0
+       move_cmd.angular.x = 0.0
+       move_cmd.angular.y = 0.0
+       move_cmd.angular.z = 0.0
+       self.publisher.publish(move_cmd)
+       rclpy.spin_once(self.node, timeout_sec=1.0)
+  
+   def __del__(self):
+       self.node.get_logger().info('Tearing down...')
+       self.node.destroy_node()
+       rclpy.shutdown()
--- pupper_llm/pupper_llm/karel/karel_chat_gpt_commander.py
+++ pupper_llm/pupper_llm/karel/karel_chat_gpt_commander.py
@@ -5,123 +5,147 @@ import pyttsx3
 from openai import OpenAI
 import karel  # Importing your KarelPupper API
 
+
 client = OpenAI(api_key='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')  # Set your OpenAI API key here
 
+
 class GPT4ConversationNode(Node):
-    def __init__(self):
-        super().__init__('gpt4_conversation_node')
+   def __init__(self):
+       super().__init__('gpt4_conversation_node')
 
-        # Create a subscriber to listen to user queries
-        self.subscription = self.create_subscription(
-            String,
-            'user_query_topic',  # Replace with your topic name for queries
-            self.query_callback,
-            10
-        )
 
-        # Create a publisher to send back responses
-        self.publisher_ = self.create_publisher(
-            String,
-            'gpt4_response_topic',  # Replace with your topic name for responses
-            10
-        )
+       # Create a subscriber to listen to user queries
+       self.subscription = self.create_subscription(
+           String,
+           'user_query_topic',  # Replace with your topic name for queries
+           self.query_callback,
+           10
+       )
 
-        self.get_logger().info('GPT-4 conversation node started and waiting for queries...')
 
-        # Initialize the text-to-speech engine
-        self.tts_engine = pyttsx3.init()
-        self.tts_engine.setProperty('rate', 150)  # Set the speed of speech (optional)
+       # Create a publisher to send back responses
+       self.publisher_ = self.create_publisher(
+           String,
+           'gpt4_response_topic',  # Replace with your topic name for responses
+           10
+       )
 
-        # Initialize KarelPupper robot control
-        self.pupper = karel.KarelPupper()
 
-    # TODO: Implement the query_callback method
-    # msg is a String message object that contains the user query. You can extract the query using msg.data
-    def query_callback(self, msg):
-        user_query = msg.data
-        response = self.get_gpt4_response(user_query)
+       self.get_logger().info('GPT-4 conversation node started and waiting for queries...')
 
-        response_msg = String()
-        response_msg.data = response
-        self.publisher_.publish(response_msg)
-        # Paste in your implementation from simple_gpt_chat.py
-        
-        # Play the response through the speaker with the play_response method
-        self.play_response(response)
-        # Parse and execute robot commands if present with the execute_robot_command method
-        self.execute_robot_command(response)
 
-    def get_gpt4_response(self, query):
-        try:
-            # Making the API call to GPT-4 using OpenAI's Python client
-            # prompt = str("using the commands move() turn_left() turn_right() bark() and stop() to control a robot. Here is an example of some code which makes a robot walk forwards then turn around and walk back \n"
-            # +"pupper.move()\n"
-            # +"pupper.bark()\n"
-            # +"pupper.turn_right()\n"
-            # +"pupper.turn_right()\n"
-            # +"pupper.move()\n")
+       # Initialize the text-to-speech engine
+       self.tts_engine = pyttsx3.init()
+       self.tts_engine.setProperty('rate', 150)  # Set the speed of speech (optional)
 
-            # Making the API call to GPT-4 using OpenAI's Python client
-            prompt = str("using the commands move() and turn(angle) to control a robot to draw a triangle. Here is an example of some code which makes a robot draw an equilateral triangle \n"
-            +"pupper.move()\n"
-            +"pupper.turn(1.047)\n"
-            +"pupper.move()\n"
-            +"pupper.turn(1.047)\n"
-            +"pupper.move()\n")
 
-            response = client.chat.completions.create(model="gpt-4",  # Model identifier, assuming GPT-4 is used
-            messages=[
-                {"role": "system", "content": prompt},
-                {"role": "user", "content": query}
-            ],
-            max_tokens=150)  # Adjust token limit based on your requirement
+       # Initialize KarelPupper robot control
+       self.pupper = karel.KarelPupper()
 
-            # Extract the assistant's reply from the response
-            gpt4_response = response.choices[0].message.content
-            return gpt4_response
 
-        except Exception as e:
-            self.get_logger().error(f"Error calling GPT-4 API: {str(e)}")
-            return "Sorry, I couldn't process your request due to an error."
+   # TODO: Implement the query_callback method
+   # msg is a String message object that contains the user query. You can extract the query using msg.data
+   def query_callback(self, msg):
+       user_query = msg.data
+       response = self.get_gpt4_response(user_query)
 
-    def play_response(self, response):
-        try:
-            # Use the TTS engine to say the response out loud
-            self.tts_engine.say(response)
-            self.tts_engine.runAndWait()
-        except Exception as e:
-            self.get_logger().error(f"Error playing response through speaker: {str(e)}")
 
-    def execute_robot_command(self, response):
-        # Convert the response to lowercase to handle case-insensitivity
-        response = response.lower()
-        self.get_logger().info(f"Response: {response}")
-        # TODO: Implement the robot command execution logic, in a large if-else statement. Your conditionals should be set based on the expected commands from GPT-4, and the corresponding methods should be called on the KarelPupper object.
-        for line in response.split("\n"):
-            line = line.strip()
-            if "pupper.move()" in line:
-                self.pupper.move()
-            elif "pupper.bark()" in line:
-                self.pupper.bark()
-            elif "pupper.turn_left()" in line:
-                self.pupper.turn_left()
-            elif "pupper.turn_right()" in line:
-                self.pupper.turn_right()
-            elif "pupper.stop()" in line:
-                self.pupper.stop()
+       response_msg = String()
+       response_msg.data = response
+       self.publisher_.publish(response_msg)
+       # Paste in your implementation from simple_gpt_chat.py
+      
+       # Play the response through the speaker with the play_response method
+       self.play_response(response)
+       # Parse and execute robot commands if present with the execute_robot_command method
+       self.execute_robot_command(response)
 
-        pass
+
+   def get_gpt4_response(self, query):
+       try:
+           # Making the API call to GPT-4 using OpenAI's Python client
+           # prompt = str("using the commands move() turn_left() turn_right() bark() and stop() to control a robot. Here is an example of some code which makes a robot walk forwards then turn around and walk back \n"
+           # +"pupper.move()\n"
+           # +"pupper.bark()\n"
+           # +"pupper.turn_right()\n"
+           # +"pupper.turn_right()\n"
+           # +"pupper.move()\n")
+
+
+           # Making the API call to GPT-4 using OpenAI's Python client
+           prompt = str("using the commands move() and turn(radians) to control a robot to draw a shape where radians must be a numeric decimal. Here is an example of some code which makes a robot draw an equilateral triangle \n"
+           +"pupper.move()\n"
+           +"pupper.turn(1.047)\n"
+           +"pupper.move()\n"
+           +"pupper.turn(1.047)\n"
+           +"pupper.move()\n")
+
+
+           response = client.chat.completions.create(model="gpt-4",  # Model identifier, assuming GPT-4 is used
+           messages=[
+               {"role": "system", "content": prompt},
+               {"role": "user", "content": query}
+           ],
+           max_tokens=150)  # Adjust token limit based on your requirement
+
+
+           # Extract the assistant's reply from the response
+           gpt4_response = response.choices[0].message.content
+           return gpt4_response
+
+
+       except Exception as e:
+           self.get_logger().error(f"Error calling GPT-4 API: {str(e)}")
+           return "Sorry, I couldn't process your request due to an error."
+
+
+   def play_response(self, response):
+       try:
+           # Use the TTS engine to say the response out loud
+           self.tts_engine.say(response)
+           self.tts_engine.runAndWait()
+       except Exception as e:
+           self.get_logger().error(f"Error playing response through speaker: {str(e)}")
+
+
+   def execute_robot_command(self, response):
+       # Convert the response to lowercase to handle case-insensitivity
+       response = response.lower()
+       self.get_logger().info(f"Response: {response}")
+       # TODO: Implement the robot command execution logic, in a large if-else statement. Your conditionals should be set based on the expected commands from GPT-4, and the corresponding methods should be called on the KarelPupper object.
+       for line in response.split("\n"):
+           line = line.strip()
+           if "pupper.move()" in line:
+               self.pupper.move()
+           elif "pupper.turn(" in line:
+               radians = float(line[line.find("(") + 1: line.find(")")])
+               self.pupper.turn(radians)
+           elif "pupper.bark()" in line:
+               self.pupper.bark()
+           elif "pupper.turn_left()" in line:
+               self.pupper.turn_left()
+           elif "pupper.turn_right()" in line:
+               self.pupper.turn_right()
+           elif "pupper.stop()" in line:
+               self.pupper.stop()
+
+
+       pass
+
 
 def main(args=None):
-    rclpy.init(args=args)
+   rclpy.init(args=args)
 
-    # Create the node and spin it
-    gpt4_conversation_node = GPT4ConversationNode()
-    rclpy.spin(gpt4_conversation_node)
 
-    # Clean up and shutdown
-    gpt4_conversation_node.destroy_node()
-    rclpy.shutdown()
+   # Create the node and spin it
+   gpt4_conversation_node = GPT4ConversationNode()
+   rclpy.spin(gpt4_conversation_node)
+
+
+   # Clean up and shutdown
+   gpt4_conversation_node.destroy_node()
+   rclpy.shutdown()
+
 
 if __name__ == '__main__':
-    main()
+   main()
